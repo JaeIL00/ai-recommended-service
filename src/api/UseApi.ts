69eb56208c7bf438d16212ed1bae30dddc1367b6
ShinJaeIl01 <ekvm555@naver.com>
2023-03-22 20:12:09 +0900

    Proxy by Netlify

--- src/api/UseApi.ts
+++ src/api/UseApi.ts
@@ -3,8 +3,10 @@ type reqBodyType = {
   max_tokens: number;
 };
 
+const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
 export const fetchAiAnswer = async (reqBody: reqBodyType) => {
-  const response = await fetch(`v1/inference/kogpt/generation`, {
+  const response = await fetch(`${PROXY}/v1/inference/kogpt/generation`, {
     method: "POST",
     headers: {
       Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_API_KEY}`,
