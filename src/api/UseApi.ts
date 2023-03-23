be244e10a493807356bcc2c9c81cf202b5f5ecd9
ShinJaeIl01 <ekvm555@naver.com>
2023-03-23 20:32:40 +0900

    API Changed to OpenAI

--- src/api/UseApi.ts
+++ src/api/UseApi.ts
@@ -1,18 +1,15 @@
 export const fetchOpenAnswer = async (userPropmpt: string) => {
-  const response = await fetch(
-    `https://api.openai.com/v1/engines/text-davinci-003/completions`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: userPropmpt,
-        temperature: 0,
-        max_tokens: 50,
-      }),
-    }
-  );
+  const response = await fetch("/v1/engines/text-davinci-003/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: userPropmpt,
+      temperature: 0,
+      max_tokens: 1000,
+    }),
+  });
   return response.json();
 };
--- src/components/SendingText.tsx
+++ src/components/SendingText.tsx
@@ -1,31 +1,21 @@
-import React, { useState } from "react";
 import { useMutation } from "react-query";
 import { fetchOpenAnswer } from "../api/UseApi";
 
 export const SendingText = () => {
-  const [saidAi, setSaidAi] = useState<string>("");
   const kogptMutation = useMutation("textToAi", fetchOpenAnswer, {
-    onError: (error) => {
-      console.log("error : ", error);
-    },
-    onSuccess: (data) => {
-      console.log("success");
-    },
+    onError: (error) => {},
+    onSuccess: (data) => {},
   });
 
-  const [userInputText, setUserInputText] = useState<string>("");
-
   const koSubmitButtonHandler = async () => {
-    kogptMutation.mutate("hi");
+    kogptMutation.mutate("");
   };
 
   return (
     <div style={{ textAlign: "center", margin: "10vw" }}>
       <h1>Short Novel</h1>
-      <h4>{kogptMutation.isLoading ? "................." : saidAi}</h4>
       <textarea
         placeholder="인간 시대의 끝이 도래했다."
-        onChange={(event) => setUserInputText(event.target.value)}
         style={{ width: "80vw", height: "10vh", resize: "none" }}
       />
       <button onClick={() => koSubmitButtonHandler()}>전송</button>
