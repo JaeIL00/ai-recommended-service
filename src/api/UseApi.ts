726dacdeb5e2560273a73ddcb4bc7e2753807bdb
January <101620064+JaeIL00@users.noreply.github.com>
2023-03-24 16:14:19 +0900

    Simple Test

--- src/api/UseApi.ts
+++ src/api/UseApi.ts
@@ -3,7 +3,7 @@
 const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY
 const JAVASCRIPT_KEY = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY
 const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URL
-export const getKakaoAuthCode = `https://kauth.kakao.com/oauth/authorize?client_id=${JAVASCRIPT_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`
+export const getKakaoAuthCode = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`
 
 export const getKakaoToken = async (KAKAO_CODE: string) => {
 	const response = await fetch('https://kauth.kakao.com/oauth/token', {
