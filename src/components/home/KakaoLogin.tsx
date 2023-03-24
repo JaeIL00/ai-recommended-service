a48b75b08f40dbea4b67a024f759ed442638703b
ShinJaeIl01 <ekvm555@naver.com>
2023-03-24 16:41:15 +0900

    자동 로그인 삭제 및 테스트

--- src/components/home/KakaoLogin.tsx
+++ src/components/home/KakaoLogin.tsx
@@ -13,9 +13,9 @@ export const KakaoLogin = () => {
 	const kakaoCode = location.search.split('=')[1]
 
 	// 마운트 후 토큰 발급 함수 호출
-	// useEffect(() => {
-	// 	mutate(kakaoCode)
-	// },[])
+	useEffect(() => {
+		mutate(kakaoCode)
+	},[])
 
 	// 인가코드로 토큰 발급
 	const {mutate} = useMutation('getKakaoToken', getKakaoToken, {
@@ -25,6 +25,7 @@ export const KakaoLogin = () => {
 				// Kakao.Auth.setAccessToken(data.access_token);
 				// localStorage.setItem('token', data.access_token)
 				// getUserInfo()
+				console.log(data.access_token)
 			}
 			else navigate('/signin')
 		}
